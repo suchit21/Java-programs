aa7d01156a5324ec18c2e88ac31fcdea3b26a521
Hari <73698945+suchit21@users.noreply.github.com>
2025-12-19 13:00:36 +0530

    Add files via upload

---
+++ Ifelsec.java
@@ -0,0 +1,26 @@
+package com.tka.dec11;
+
+import java.util.Scanner;
+
+public class Ifelsec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//     Scanner sc= new Scanner(System.in);
+//     int a = sc.nextInt();
+		int a = 15;
+     if (a%3==0) {
+    	 System.out.println("FIZZ");} 
+      if (a%5==0) {                       
+    	                                  
+    	                                     
+    		 System.out.println("BUZZ");
+    	 }
+     if (a%3==0 && a%5==0 ) {
+    		 System.out.println("FIZZBUZZ");
+    	 }
+     }
+     
+	}
+
+
